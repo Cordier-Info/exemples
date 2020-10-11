5419e1529d039186603c0940c60a00c7303dd82c
Prof Info <66843772+Cordier-Info@users.noreply.github.com>
2020-10-11 10:57:54 +0200

    -

--- pendu.py
+++ pendu.py
@@ -1,27 +1,29 @@
 from dessins_pendu import *
 import getpass
 
-nb_erreur = 0
+nb_erreur, tour = 0 , 0
 mot = getpass.getpass().upper()
 print()
-mot_partiel = list('_'*(len(mot)))
-print("Mot mystère : "+' '.join(mot_partiel))
+mot_partiel = list('_' * (len(mot)))
+print("Mot mystère : " + ' '.join(mot_partiel))
 
-while "_" in mot_partiel :
-    lettre = input("\n\nChosir une lettre : ").upper()
-    if lettre in mot :
+while "_" in mot_partiel:
+    print("\n"+"#"*10,"Tour",tour,"#"*10)
+    lettre = input("\nChosir une lettre : ").upper()
+    if lettre in mot:
         print("\nBon choix !\n")
-        for i,l in enumerate(mot) :
-            if l == lettre :
+        for i, l in enumerate(mot):
+            if l == lettre:
                 mot_partiel[i] = lettre
-    else :
+    else:
         nb_erreur += 1
         print("\nRaté !")
         print(dessin[nb_erreur])
-        if nb_erreur == 7 :
+        if nb_erreur == 7:
             print(">>> G A M E  O V E R <<<\n")
             break
-    print("Mot mystère : "+' '.join(mot_partiel))
+    print("Mot mystère : " + ' '.join(mot_partiel))
+    tour += 1
 
-else :
+else:
     print("\n\n>>> B R A V O ! <<<\n")
