4f4553df3e4f8fe540066fff8d234f220607415b
Prof Info <66843772+Cordier-Info@users.noreply.github.com>
2020-10-23 15:20:39 +0200

    -

--- codecorr.py
+++ codecorr.py
@@ -8,8 +8,8 @@ mots = []
 G = np.array([[1,1,0,1,1,0,1,0,0],
                 [0,1,0,0,0,1,1,1,1],
                 [1,0,1,0,0,0,1,1,1]])
-
-print("G =\n",G,end="\n\n")
+print("G =")
+print(G)
 print()
 
 # liste des mots codes :
@@ -26,7 +26,7 @@ print()
 G[0] += G[1]
 G[2] += G[0]
 G %= 2
-print("G =")
+print("Gech =")
 print(G)
 print()
 
@@ -38,31 +38,30 @@ print()
 
 # on en déduit la matrice de parité H
 Pt = P.transpose()
-H = np.concatenate((Pt, np.identity(6)), axis=1)
+H = np.concatenate((Pt, np.identity(6)), axis=1).astype(np.uint8)
 print("H =")
 print(H)
 print()
 
 # on imprime les syndromes et les éléments de la classe correspondante
-print("syndromes et classe (une erreur) :")
+print("syndromes et classes associées (une erreur) :")
 for i in range(n) :
     e = np.zeros(n)
     e[i] = 1
     s = np.dot(e,H.transpose())%2
-    print("s{} = ".format(i),s)
+    print("s{} = ".format(i),s.astype(np.uint8))
     for m in mots :
-        print((m+e)%2, end=' ')
+        print(((m+e)%2).astype(np.uint8), end=' ')
     print()
 
 print()
-
-print("syndromes et classe (deux erreurs dont une en première position) :")
+print("syndromes et classes associées (deux erreurs dont une en première position) :")
 for i in range(1,n) :
     e = np.zeros(n)
     e[0] = 1
     e[i] = 1
     s = np.dot(e,H.transpose())%2
-    print("s{} = ".format(i),s)
+    print("s{} = ".format(i),s.astype(np.uint8))
     for m in mots :
-        print((m+e)%2, end=' ')
+        print(((m+e)%2).astype(np.uint8), end=' ')
     print()
