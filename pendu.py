56c1dbfe398d457d8ed3b627d636cefb4ab4d08e
Prof Info <66843772+Cordier-Info@users.noreply.github.com>
2020-12-12 00:49:59 +0100

    Update pendu.py

--- pendu.py
+++ pendu.py
@@ -6,24 +6,28 @@ mot = getpass.getpass().upper()
 print()
 mot_partiel = list('_' * (len(mot)))
 print("Mot mystère : " + ' '.join(mot_partiel))
+alzheimer = []
 
 while "_" in mot_partiel:
     print("\n"+"#"*10,"Tour",tour,"#"*10)
     lettre = input("\nChosir une lettre : ").upper()
-    if lettre in mot:
+    if (lettre in mot) and (lettre not in alzheimer) :
         print("\nBon choix !\n")
         for i, l in enumerate(mot):
             if l == lettre:
                 mot_partiel[i] = lettre
     else:
         nb_erreur += 1
-        print("\nRaté !")
+        if lettre in alzheimer :
+            print("\nLettre déjà donnée...")
+        else :
+            print("\nRaté !")
         print(dessin[nb_erreur])
         if nb_erreur == 7:
             print(">>> G A M E  O V E R <<<\n")
             break
     print("Mot mystère : " + ' '.join(mot_partiel))
     tour += 1
-
+    alzheimer.append(lettre)
 else:
     print("\n\n>>> B R A V O ! <<<\n")
